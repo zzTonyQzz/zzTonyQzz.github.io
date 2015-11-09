181ab800c09e267635858c8a9f69c4c376407afd
hqxuLibnet <hqxu@libnet.sh.cn>
2015-11-09 17:17:52 +0800

    修正ie下兼容性 仍待改进

--- assets/plugins/backstretch/backstretch-ini.js
+++ assets/plugins/backstretch/backstretch-ini.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $.getScript("assets/plugins/backstretch/jquery.backstretch.min.js", function() {
-    $(".fullscreen-static-image").backstretch("assets/img/bg/img1.jpg");
+    $(".fullscreen-static-image").backstretch("assets/img/sliders/revolution/bg1.jpg");
   });
 });
