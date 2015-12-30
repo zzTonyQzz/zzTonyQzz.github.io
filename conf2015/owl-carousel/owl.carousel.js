2d1425f14f463f7698ab18067964ff87314d85ac
TonyQ <525160324@qq.com>
2015-12-30 16:41:34 +0800

    updateconf2015

--- conf2015/owl-carousel/owl.carousel.js
+++ conf2015/owl-carousel/owl.carousel.js
@@ -1469,7 +1469,7 @@ if (typeof Object.create !== "function") {
         stopOnHover : false,
 
         navigation : false,
-        navigationText : ["prev", "next"],
+        navigationText : ["上一个", "下一个"],
         rewindNav : true,
         scrollPerPage : false,
 
