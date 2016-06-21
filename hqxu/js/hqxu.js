e321dc28ae78e6991b7dbbf3faaf9606707b4055
TonyQ <525160324@qq.com>
2016-06-21 12:57:33 +0800

    sorry for willing

---
+++ hqxu/js/hqxu.js
@@ -0,0 +1,12 @@
+/*
+* Copyright 2014, TonyQ[HqXu]
+* hqxu@libnet.sh.cn
+* 64455555*3537
+* Date: 2015/03/10
+*/
+;
+if (window.console && console.log) {
+	console.log("********************");
+	console.log("如果您发现代码存在问题或有任何建议，请联系 TonyQ[hqxu@libnet.sh.cn] ，期待您的帮助！");
+	console.log("********************");
+};
